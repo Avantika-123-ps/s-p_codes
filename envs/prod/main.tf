0701026f47c5b4627d8b278d2aa759909acdb093
Avantika Pandey <avantikapandey@google.com>
2026-01-28 16:23:48 +0000

    del backend.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -8,10 +8,14 @@ module "log_infrastructure" {
   project_id = var.project_id
 
   buckets_list = [for b in local.buckets_data : {
-    name          = b.name
-    location      = b.location
-    versioning    = tobool(b.versioning)
-    log_sink_name = try(b.log_sink_name, null)
+    name                 = b.name
+    location             = b.location
+    versioning           = tobool(b.versioning)
+    log_sink_name        = try(b.log_sink_name, null)
+    retention_days       = try(tonumber(b.retention_days), null)
+    filter               = try(b.filter, null)
+    parent_resource_id   = try(b.parent_resource_id, null)
+    parent_resource_type = try(b.parent_resource_type, null)
   }]
 }
 
